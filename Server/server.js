6fecf21b89d3c1bd9bf38a81b84dd8a3a5e30ea2
leckaraxmuffin <l.maslowski@gmx.at>
2021-06-17 13:21:15 +0200

    Server ueberarbeitet

--- Server/server.js
+++ Server/server.js
@@ -3,22 +3,26 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 let anz = 0;
+let daten;
 
 app.listen(port, () => {
     console.log("server läuft unter http://localhost:" + port);
 });
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     anz++;
     console.log(anz);
-    
+    res.send(' Hi ');
+});
 
+app.get('/info', (req, res) => {
+    //let anzMenschen = parseInt(daten)
     fs.readFile('../py/readme.txt', (error, data) => {
         daten = JSON.stringify(data); 
         console.log(daten);
-        res.send(daten);
+        //Daten auslesen muss noch in Public rein - JSON.parse(document.all[2].innerHTML).data
     });
-});
-
-app.use(express.static('public'));
-
+    res.send(daten);
+})
