0ece8e198f7823389df6bc998d1a875d9fa5e928
leckaraxmuffin <l.maslowski@gmx.at>
2021-06-17 13:03:51 +0200

    Server ueberarbeitung

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,24 @@
-
 const express = require("express");
+const fs = require('fs');
 const app = express();
 const port = 3000;
-app.listen(port, ()=> {
-    console.log("server läuft unter http://localhost:"+port);
-})
+let anz = 0;
+
+app.listen(port, () => {
+    console.log("server läuft unter http://localhost:" + port);
+});
+
+app.get('/', (req, res) => {
+    anz++;
+    console.log(anz);
+    
+
+    fs.readFile('../py/readme.txt', (error, data) => {
+        daten = JSON.stringify(data); 
+        console.log(daten);
+        res.send(daten);
+    });
+});
 
 app.use(express.static('public'));
+
