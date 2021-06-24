16e8cecbe444e3fa5ffa55d9d47221f78d7532e0
Mara7571 <71823686+Mara7571@users.noreply.github.com>
2021-06-24 12:48:32 +0200

    Add files via upload

---
+++ Server/public/chart.js
@@ -0,0 +1,27 @@
+//chart js
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'],
+        datasets: [{
+            label: 'Anzahl der Besucher',
+            data: [50, 1000, 1500, 2300, 3200, 1800, 1600, 2000, 2500, 1900, 800, 100, 0],
+            fill: false,
+    borderColor: 'rgb(80, 79, 79)',
+    tension: 0.1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
